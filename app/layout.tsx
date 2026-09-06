9d7426b54ce0de3d77f8e70449a88baacccd44fe
Ayo Richard ABE [gODtECH] <ayoabe24@gmail.com>
2026-09-06 22:45:54 +0100

    feat: mount mark-all-read mailbox control

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import './responsive-fix.css';
 import './mail-view.css';
 import NotificationWatcher from '@/components/notification-watcher';
+import MarkAllRead from '@/components/mark-all-read';
 
 export const metadata: Metadata = {
   title: 'ABEmail Mail',
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body>
         {children}
         <NotificationWatcher />
+        <MarkAllRead />
       </body>
     </html>
   );
