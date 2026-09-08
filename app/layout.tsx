3a856e360b59b4d9a944a97cc7e27565a1340528
Ayo Richard ABE [gODtECH] <ayoabe24@gmail.com>
2026-09-08 12:35:52 +0100

    fix: expose Scheduled Messages in mailbox navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import AttachmentsController from '@/components/attachments-controller';
 import SearchNavigator from '@/components/search-navigator';
 import MarkAllRead from '@/components/mark-all-read';
 import ReportIssue from '@/components/report-issue';
+import ScheduledNavLink from '@/components/scheduled-nav-link';
 
 export const metadata: Metadata = {
   title: 'ABEmail Mail',
@@ -26,6 +27,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <SearchNavigator />
         <MarkAllRead />
         <ReportIssue />
+        <ScheduledNavLink />
       </body>
     </html>
   );
